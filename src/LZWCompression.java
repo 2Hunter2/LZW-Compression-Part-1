9bc93a838dcccecfa1c11e48ddd9be0038079160
2Hunter2 <hunter1griswold@gmail.com>
2021-09-08 14:33:25 -0700

    Initial commit

---
+++ src/LZWCompression.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class LZWCompression {
+	ArrayList<String> arr = new ArrayList<String>();
+	for (int k = 0; k < 256; k ++)
+	{
+		arr.add ((char)k + "");
+		
+	}
+	
+}
