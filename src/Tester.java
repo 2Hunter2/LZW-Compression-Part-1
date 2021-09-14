d8b639af66d8abed5534608f219ae05b6d39deb5
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-09-13 18:43:36 -0700

    Made a Tester class

--- src/Tester.java
+++ src/Tester.java
@@ -1,4 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
-public class Tester {
+public class Tester
+{
+	public static void main(String[] args) throws Exception
+	{
+		LZWCompression compressor = new LZWCompression ();
 
+		
+		BufferedReader reader = new BufferedReader(new FileReader("lzw-file1.txt"));
+		
+		String str = null;
+		
+		while (reader.ready())
+		{
+			str = str + reader.read();
+		}
+		
+		compressor.fileCompression (str);
+		compressor.compress(str);
+		
+		
+	}
 }
